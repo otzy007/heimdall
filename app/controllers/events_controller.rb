3989cb7b7a8c589b52baf4ae3f6037df13cb7bae
Andrei Gliga <otzy_007@yahoo.com>
2014-11-26 23:14:06 +0200

    search the events in bucharest

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
   def index
+    @events = FbGraph::Event.search('Bucharest', access_token: current_user.token, fields: 'cover,name,description,venue,start_time,picture')
   end
 end
