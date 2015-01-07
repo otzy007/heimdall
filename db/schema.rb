e3a0f56f96d0f81f3c567b3a608ba39161d49c25
Andrei Gliga <otzy_007@yahoo.com>
2015-01-07 19:09:14 +0200

    category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128101155) do
+ActiveRecord::Schema.define(version: 20150107170816) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.text     "keywords"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "event_filters", force: true do |t|
     t.integer  "event_id"
