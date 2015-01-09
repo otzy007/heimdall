d539618cb5544a96c6ca0afac4388629c4ca8349
Andrei Gliga <otzy_007@yahoo.com>
2015-01-09 10:31:29 +0200

    event_filter belongs to the user

--- app/models/event_filter.rb
+++ app/models/event_filter.rb
@@ -3,4 +3,7 @@ class EventFilter < ActiveRecord::Base
   #
   # hide: never show again that event
   # like: preffer events like this one
+  # dislike: bring down to the list the event
+
+  belongs_to :user
 end
